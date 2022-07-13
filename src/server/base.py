0f1d68376f1a12c29d591fb219a1cf33fddc1cd5
amin-egn <aminx15am@yahoo.com>
2022-07-13 16:03:01 +0430

    add server base factory to spam on server

---
+++ src/server/base.py
@@ -0,0 +1,36 @@
+# fake
+from faker import Faker
+
+
+class Factory(object):
+    """Factory"""
+    def __init__(self, conn, url, headers):
+        self.conn = conn
+        self.headers = headers
+        self.url = url
+        self.fake = Faker()
+
+    @property
+    def cursor(self):
+        return self.conn.cursor()
+
+    def create(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def update(self):
+        pass
+
+    def create_more(self, n):
+        for _ in range(0, n):
+            self.create()
+
+    def delete_more(self, n):
+        for _ in range(0, n):
+            self.delete()
+
+    def update_more(self, n):
+        for _ in range(0, n):
+            self.update()
