c46d20491b9d593278626a22626011ef8b7035c6
amin-egn <aminx15am@yahoo.com>
2022-07-13 16:24:41 +0430

    refactor main, make main and first_time functions and some minor changes

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import random
 import time
 # internal
 from src import connection
-from src.product import ProductFactory
-from src.person import PersonFactory
-from src.product import QuantityFactory
+from src.local.product import ProductFactory
+from src.local.person import PersonFactory
+from src.server.invoices import InvoiceWebFactory
+from src.local.portions import PortionFactory
 
 
 confs = {
@@ -16,67 +17,90 @@ confs = {
 }
 conn = connection.get(**confs)
 
-product = ProductFactory(conn)
+HEADERS = {
+    'Authorization': "Token 1b53627f436cd58ae74aa7764f164bb6f0d462b1"
+}
+URL = 'http://127.0.0.1:8000'
+
 person = PersonFactory(conn)
+product = ProductFactory(conn)
+invoice = InvoiceWebFactory(conn, URL, HEADERS)
+portion = PortionFactory(conn)
+
+factories = (
+    person,
+    product,
+    invoice,
+    portion,
+)
+
+track_tuple_key = (
+    'Person',
+    'Product',
+    'Invoice',
+    'Portion'
+)
+track_tuple_value = (
+    'COUNT',
+    'DELETE',
+    'INSERT',
+    'UPDATE',
+)
 
 
-if __name__ == '__main__':
-    time_inter = int(input('how much time sleep? '))
-    first_time = True
-    action_nums = {
-        'Product': {
-            'DELETE': 0,
-            'INSERT': 0,
-            'UPDATE': 0,
-            'COUNT': 0
-        },
-        'Person': {
-            'DELETE': 0,
-            'INSERT': 0,
-            'UPDATE': 0,
-            'COUNT': 0
-        }
-    }
+track_dict = dict.fromkeys(track_tuple_key, None)
+for k in track_dict.keys():
+    track_dict[k] = dict.fromkeys(track_tuple_value, 0)
+
+
+def first_time():
+    for i, factory in enumerate(factories):
+        if i < 2:
+            nums = 50
+        else:
+            nums = 10
+        factory.create_more(nums)
+
+
+def main(timesleep):
     while True:
-        table_name = ''
-        how_many = random.randint(1, 11)
+        how_many = random.randint(1, 8)
         what_to_do = random.choices([1, 2, 3], [1, 5, 2])[0]
-        chosen = random.choice([1, 2])
-        if chosen == 1:
-            table_name = 'Person'
-            table = person
-        elif chosen == 2:
-            table_name = 'Product'
-            table = product
-
-        if first_time:
-            how_many = 12
-            what_to_do = 2
-        nums = action_nums.get(table_name).get('COUNT')
+        chosen = random.choices([0, 1, 2, 3], [10, 10, 5, 2])[0]
+        table = factories[chosen]
+        table_name = track_tuple_key[chosen]
+        act = track_tuple_value[what_to_do]
         try:
             if what_to_do == 1:
-                act = 'DELETE'
                 table.delete_more(how_many)
             elif what_to_do == 3:
-                act = 'UPDATE'
                 table.update_more(how_many)
             else:
-                act = 'INSERT'
                 table.create_more(how_many)
-            n = action_nums.get(table_name).get(act)
-            action_nums[table_name][act] = n + how_many
 
+            n = track_dict.get(table_name).get(act)
+            track_dict[table_name][act] = n + how_many
+            nums = track_dict.get(table_name).get('COUNT')
             if act == 'DELETE':
-                action_nums[table_name]['COUNT'] = nums - how_many
+                track_dict[table_name]['COUNT'] = nums - how_many
+
             elif act == 'INSERT':
-                action_nums[table_name]['COUNT'] = nums + how_many
+                track_dict[table_name]['COUNT'] = nums + how_many
 
         except Exception as e:
             print(str(e))
 
-        first_time = False
         print(
             f'{how_many} {act} in table {table_name}\n',
-              action_nums[table_name],'\n'
+              track_dict[table_name],'\n'
             )
-        time.sleep(time_inter)
+        time.sleep(timesleep)
+
+
+if __name__ == '__main__':
+    time_interval = int(input('how much time sleep? '))
+    ft = True
+    if ft:
+        print('first time activated')
+        first_time()
+    main(time_interval)
