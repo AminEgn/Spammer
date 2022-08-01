6beb498500e9f819a31df9bba2a11d42c1fc0c6f
amin-egn <aminx15am@yahoo.com>
2022-08-01 16:10:24 +0430

    main -> 1) confs, headers and url now read from setting json file 2) reduce number of spams

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 import time
 # internal
 from src import connection
+from src import settings as stg
 from src.local.product import ProductFactory
 from src.local.person import PersonFactory
 from src.server.invoices import InvoiceWebFactory
@@ -10,17 +11,17 @@ from src.local.portions import PortionFactory
 
 
 confs = {
-    'server': '.\\Moein',
-    'username': 'sa',
-    'password': 'arta0@',
-    'database': 'Moein'
+    'server': stg.r('database_server'),
+    'username': stg.r('database_username'),
+    'password': stg.r('database_password'),
+    'database': stg.r('database_name')
 }
 conn = connection.get(**confs)
 
 HEADERS = {
-    'Authorization': "Token 1b53627f436cd58ae74aa7764f164bb6f0d462b1"
+    'Authorization': f"Token {stg.r('api_token')}"
 }
-URL = 'http://127.0.0.1:8000'
+URL = stg.r('api_domain')
 
 person = PersonFactory(conn)
 product = ProductFactory(conn)
@@ -61,10 +62,9 @@ def first_time():
             nums = 10
         factory.create_more(nums)
 
-
 def main(timesleep):
     while True:
-        how_many = random.randint(1, 8)
+        how_many = random.randint(1, 6)
         what_to_do = random.choices([1, 2, 3], [1, 5, 2])[0]
         chosen = random.choices([0, 1, 2, 3], [10, 10, 5, 2])[0]
         table = factories[chosen]
