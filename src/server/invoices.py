16ad0abd3040d6202c010e67af1d01db7d2817ff
amin-egn <aminx15am@yahoo.com>
2022-08-01 15:58:00 +0430

    server/invoices -> all fake numbers now stick together

--- src/server/invoices.py
+++ src/server/invoices.py
@@ -40,32 +40,40 @@ class InvoiceWebFactory(Factory):
         return invoice_item.create()
 
     def create(self):
+        items_total = random.randint(10000, 10000000)
+        visitor_percent = random.uniform(0.1, 0.5)
+        visitor_price = items_total * visitor_percent
+        discount_percent = random.uniform(0.1, 0.5)
+        discount_price = items_total * discount_percent
+        tax_percent = random.uniform(0.01, 0.1)
+        tax_price = items_total * tax_percent
+        total = items_total - visitor_price - discount_price + tax_price
+
         params_dict = {
             'type': random.choice([0, 2]),
             'info': self.fake.text(),
-            'total': random.randint(10000, 10000000),
-            'items_total': random.randint(10000, 10000000),
-            'remain': random.randint(10000, 10000000),
-            'tax': random.randint(10000, 1000000),
-            'tax_percent': random.randint(1, 10),
-            'visitor_price': random.randint(100, 1000),
-            'visitor_percent': random.randint(1, 50),
-            'discount': random.randint(200, 5000),
-            'discount_percent': random.randint(1, 50),
+            'total': int(total),
+            'items_total': items_total,
+            'remain': random.randint(10000, 1000000),
+            'tax': int(tax_price),
+            'tax_percent': tax_percent,
+            'visitor_price': int(visitor_price),
+            'visitor_percent': visitor_percent,
+            'discount': int(discount_price),
+            'discount_percent': discount_percent,
             'items_discount': random.randint(1000, 500000),
             'address': self.fake.address(),
             'person': self.get_person(),
             'visitor': self.get_visitor(),
             'driver': self.get_driver(),
-            'items': [self.get_items() for _ in range(random.randint(2, 10))]
+            'items': [self.get_items() for _ in range(random.randint(2, 8))]
         }
         self._post(self.url, params_dict)
 
 
 class InvoiceItemWebFactory(Factory):
     """Invoice Item Web Factory"""
-    @staticmethod
-    def _recieve(url):
+    def _recieve(self, url):
         res, data = req.get(f'{url}', headers=self.headers)
         return res, json.loads(data.text)
 
@@ -82,11 +90,16 @@ class InvoiceItemWebFactory(Factory):
         return random.choice(products.fetchall())[0]
 
     def create(self):
+        price = random.randint(1000, 100000)
+        quantity = random.randint(1, 30)
+        product = self.get_product()
+        repo = self.get_repo()
+
         params_dict = {
-            "price": random.randint(1000, 100000),
-            "quantity": random.randint(1, 30),
+            "price": price,
+            "quantity": quantity,
             "quantity1": 0,
-            "quantity2": random.randint(1, 30),
+            "quantity2": quantity,
             "unit2_in_unit1": 0,
             "visitor_price": random.randint(100, 1000),
             "visitor_percent": random.randint(1, 50),
@@ -98,7 +111,7 @@ class InvoiceItemWebFactory(Factory):
             "value": random.randint(10000, 1000000),
             "info": self.fake.text(),
             "price_level": self.get_pricelevel(),
-            "product": self.get_product(),
-            "repository": self.get_repo()
+            "product": product,
+            "repository": repo
         }
         return params_dict
